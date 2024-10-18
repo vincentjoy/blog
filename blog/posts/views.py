45a92df334b463b4596f63a637290acde00d8d24
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-18 18:49:53 +0530

    added the first template

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
 
 def home(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
 
 def post(request, id):
     return HttpResponse(id)
