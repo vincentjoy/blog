bef7c0bf04af33ae99a5aad62eb76eb824fc124f
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-19 17:09:59 +0530

    added the for tags

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
+posts = [
+    {
+        "id":0,
+        "title": "Post Title 1",
+        "content": "Post 1 description"
+    },
+    {
+        "id":1,
+        "title": "Post Title 2",
+        "content": "Post 2 description"
+    },
+    {
+        "id":2,
+        "title": "Post Title 3",
+        "content": "Post 3 description"
+    }
+]
+
 def home(request):
-    template = loader.get_template('home.html')
-    return HttpResponse(template.render())
+    return render(request, 'posts/home.html', {'posts': posts})
 
 def post(request, id):
     return HttpResponse(id)
