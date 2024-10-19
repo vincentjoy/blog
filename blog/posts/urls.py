10f6170b9a118d0e27536609570821059039ad85
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-19 18:17:26 +0530

    url tag added for back action to home from blog post page

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
+    path('home/', views.home, name='home'),
     path('<int:id>/', views.post),
     path('google/', views.google),
 ]
