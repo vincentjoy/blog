c1c361ea03615c5deacbf1ca8c6cb34c270191e2
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-22 14:58:39 +0530

    made changes in the model and re applied the migrations

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Post(models.Model):
     post_title = models.CharField(max_length=60)
     post_content = models.TextField()
-    published_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateField(auto_now=True)
 
