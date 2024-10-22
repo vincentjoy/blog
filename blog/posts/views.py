6b511f859657a85fba176132b44ae9bd06013451
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-22 23:55:41 +0530

    fetched and render data using ORM commands

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,31 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound, Http404
 from django.template import loader
-
-posts = [
-    {
-        "id":0,
-        "title": "Post Title 1",
-        "content": "Post 1 description"
-    },
-    {
-        "id":1,
-        "title": "Post Title 2",
-        "content": "Post 2 description"
-    },
-    {
-        "id":2,
-        "title": "Post Title 3",
-        "content": "Post 3 description"
-    }
-]
+from .models import Post
 
 def home(request):
-    return render(request, 'posts/index.html', {'posts': posts})
+    all_posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts': all_posts})
 
 def post(request, id):
     valid_id = False
-    for post in posts:
+    all_posts = Post.objects.all()
+    for post in all_posts:
         if post['id'] == id:
             post_dict = post
             valid_id = True
