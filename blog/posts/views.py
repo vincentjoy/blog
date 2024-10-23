7811bf4725206fd4273b492c8212b0bd4ed32a09
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-23 13:12:25 +0530

    modified the post function using get_object_or_404 method

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponseNotFound, Http404
 from django.template import loader
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 def home(request):
     all_posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': all_posts})
 
 def post(request, id):
-    try:
-        post = Post.objects.get(id=id)
-        return render(request, 'posts/post.html', {'post_dict': post})
-    except:
-        raise Http404()
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'posts/post.html', {'post_dict': post})
