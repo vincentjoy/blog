ac51838650f320e845f4a7e246cd4ef4878aa256
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-26 20:07:02 +0530

    created a custom UserCreationForm class to add email, frist name and last name fields to the form

--- blog/accounts/views.py
+++ blog/accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 from django.urls import reverse
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             homeurl = reverse('home')
             return HttpResponseRedirect(homeurl)
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(request, 'accounts/register.html', {
         'form': form
