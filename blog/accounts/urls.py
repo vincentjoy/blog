fb6631727ff3b8c83457f1635c284680d64a7179
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-29 15:11:15 +0530

    redirecting to users to login and registration pages, from home page, on the corresponding button actions

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register),
-    path('login/', views.auth_login)
+    path('register/', views.register, name='register'),
+    path('login/', views.auth_login, name='login')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,11 +5,8 @@ from .models import Post
 from django.shortcuts import get_object_or_404
 
 def home(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect('/accounts/login')
-    else:
-        all_posts = Post.objects.all()
-        return render(request, 'posts/index.html', {'posts': all_posts})
+    all_posts = Post.objects.all()
+    return render(request, 'posts/index.html', {'posts': all_posts})
 
 def post(request, id):
     post = get_object_or_404(Post, id=id)
