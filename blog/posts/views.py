7af23889bec5344b5e1268feee4e8b4a829529ef
Vincent Joy <vincentjoyhere@gmail.com>
2024-10-29 14:52:06 +0530

    redirecting non authenticated users to home page, if login is accessed

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound, Http404
+from django.http import HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.template import loader
 from .models import Post
 from django.shortcuts import get_object_or_404
 
 def home(request):
-    all_posts = Post.objects.all()
-    return render(request, 'posts/index.html', {'posts': all_posts})
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/accounts/login')
+    else:
+        all_posts = Post.objects.all()
+        return render(request, 'posts/index.html', {'posts': all_posts})
 
 def post(request, id):
     post = get_object_or_404(Post, id=id)
