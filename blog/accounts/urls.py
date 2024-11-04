b9d74737ecaf1682818818cc637af52ab26356b0
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-04 19:44:30 +0530

    redirecting authenticated user to home screen if login is accessed

--- blog/accounts/urls.py
+++ blog/accounts/urls.py
@@ -5,6 +5,6 @@ from .forms import LoginForm
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name="accounts/login.html", authentication_form=LoginForm), name='login'),
+    path('login/', LoginView.as_view(template_name="accounts/login.html", authentication_form=LoginForm, redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
