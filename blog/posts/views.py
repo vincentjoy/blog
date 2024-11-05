07767530827dafcd99099d16779b7b2c807e4578
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-05 09:50:25 +0530

    logic to make previous and next button dynamic, added

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -8,7 +8,8 @@ from django.core.paginator import Paginator
 def home(request):
     all_posts = Post.objects.all().order_by('-id')
     paginator = Paginator(all_posts, 4)
-    page_obj = paginator.get_page(3)
+    page_number = request.GET.get('p', 1)
+    page_obj = paginator.get_page(page_number)
     return render(request, 'posts/index.html', {'posts': page_obj})
 
 def post(request, id):
