c87de362117bbfd53dc8466eb2faa873ae1de12d
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-05 11:20:12 +0530

    added the orphan parameter

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -7,7 +7,7 @@ from django.core.paginator import Paginator
 
 def home(request):
     all_posts = Post.objects.all().order_by('-id')
-    paginator = Paginator(all_posts, 4)
+    paginator = Paginator(all_posts, 4, orphans=2)
     page_number = request.GET.get('p', 1)
     page_obj = paginator.get_page(page_number)
     return render(request, 'posts/index.html', {'posts': page_obj})
