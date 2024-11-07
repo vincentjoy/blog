62a2de32095d0b4f7681cebf65db6f195fb7ba72
Vincent Joy <vincentjoyhere@gmail.com>
2024-11-07 22:30:15 +0530

    converted the home and post function based views to class based views

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('<int:id>/', views.post, name='post'),
+    # path('', views.home, name='home'),
+    # path('<int:id>/', views.post, name='post'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('<int:pk>/', views.PostView.as_view(), name='post'),
     path('tags/<int:id>/', views.tags, name='tag'),
     path('search/', views.search, name='search'),
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -9,12 +9,21 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import Q
 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 def home(request):
     all_posts = Post.objects.all().order_by('-id')
     paginator = Paginator(all_posts, 4, orphans=2)
     page_number = request.GET.get('p', 1)
     page_obj = paginator.get_page(page_number)
     return render(request, 'posts/index.html', {'posts': page_obj, 'total': all_posts.count()})
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    ordering = ['-id']
+    context_object_name = 'posts'
 
 def post(request, id):
     post = get_object_or_404(Post, id=id)
@@ -29,6 +38,28 @@ def post(request, id):
             return HttpResponseRedirect(posturl)
     form = CommentForm()
     return render(request, 'posts/post.html', {'post_dict': post, 'form': form, 'comments': post.comment_set.all()})
+
+class PostView(DetailView):
+    model = Post
+    template_name = 'posts/post.html'
+    context_object_name = 'post_dict'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm
+        context['comments'] = self.object.comment_set.all()
+        return context
+
+    def post(self, request, pk):
+        self.object = self.get_object()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = self.object
+            comment.user = request.user
+            comment.save()
+            posturl = reverse('post', args=[self.object.id])
+            return HttpResponseRedirect(posturl)
 
 def tags(request, id):
     tag = Tag.objects.get(id=id)
